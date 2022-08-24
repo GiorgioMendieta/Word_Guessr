342a968c94b2c07357bd218289b23d2e3fbc0374
Giorgio <carscars21@gmail.com>
2022-08-24 16:42:44 -0500

    Implemented GET & POST methods for parameters

--- app.py
+++ app.py
@@ -11,25 +11,43 @@ app.config["DEBUG"] = True
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Show main game"""
-    NUM_GUESSES = 6
-    NUM_LETTERS = 5
-
-    tiles = [[0] * NUM_LETTERS for i in range(NUM_GUESSES)]
-
-    for guess in range(NUM_GUESSES):
-        for letter in range(NUM_LETTERS):
-            tiles[guess][letter] = ''
 
     # Keyboard layout
     keys = [['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
             [' ', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ' '],
             ['Enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Back']]
 
+    if request.method == "POST":
+
+        NUM_LETTERS = request.form.get("letters")
+        if not NUM_LETTERS:
+            NUM_LETTERS = 5  # Default value
+
+        NUM_LETTERS = int(NUM_LETTERS)
+
+        NUM_GUESSES = request.form.get("guesses")
+        if not NUM_GUESSES:
+            NUM_GUESSES = 6  # Default value
+
+        NUM_GUESSES = int(NUM_GUESSES)
+
+    else :
+        # Default values
+        NUM_LETTERS = 5
+        NUM_GUESSES = 6
+    
+    # Create board and populate it with blank values
+    tiles = [[0] * NUM_LETTERS for i in range(NUM_GUESSES)]
+
+    for guess in range(NUM_GUESSES):
+        for letter in range(NUM_LETTERS):
+            tiles[guess][letter] = ''
+
     return render_template("index.html",
-                           keys=keys,
-                           num_guesses=NUM_GUESSES,
-                           num_letters=NUM_LETTERS,
-                           tiles=tiles)
+                        keys=keys,
+                        num_guesses=NUM_GUESSES,
+                        num_letters=NUM_LETTERS,
+                        tiles=tiles)
