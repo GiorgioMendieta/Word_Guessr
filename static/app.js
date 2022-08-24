5f7b2aed8b79b0e98c1a66765f07008d70e1288c
Giorgio <carscars21@gmail.com>
2022-08-24 15:46:36 -0500

    Fixed score sharing

--- static/app.js
+++ static/app.js
@@ -396,7 +396,7 @@ function shareScore() {
     let msg;
 
     // Last attempt without guessing the wordle?
-    if (guess == (NUM_GUESSES - 1)) {
+    if (guess >= (NUM_GUESSES - 1)) {
         msg = `X/${NUM_GUESSES} attempts 😢\n`;
     } else {
         msg = `${guess + 1}/${NUM_GUESSES} attempts 😎\n`;
